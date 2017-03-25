953e44577325daed501e3a3f077eaa7ce7ab484d
aminaettayebi <aminaettayebi@dhcp-10-22-47-201.wlan.ntnu.no>
2017-03-25 11:28:20 +0100

    added comment

--- app/src/main/java/com/example/ingridstoen/alarme/CalendarActivity.java
+++ app/src/main/java/com/example/ingridstoen/alarme/CalendarActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CalendarActivity extends AppCompatActivity {
+    //hallo amina
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
