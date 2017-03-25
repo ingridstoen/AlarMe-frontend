bcae271eb9cb4c420ca0fe6326aae273deef5bf3
ingridstoen <stoeningrid@gmail.com>
2017-03-25 15:38:16 +0100

    Lagt til separate skjermer for login og registrer

--- app/src/main/java/com/example/ingridstoen/alarme/MainActivity.java
+++ app/src/main/java/com/example/ingridstoen/alarme/MainActivity.java
@@ -1,61 +1,59 @@
 package com.example.ingridstoen.alarme;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.content.Intent;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import java.sql.PreparedStatement;
-import java.sql.*;
-import android.util.Log;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import  java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    String username, password;
+    Button button;
     Button bLogin;
-    EditText etbrukernavn, etpassord;
+    EditText edit_username, edit_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etbrukernavn = (EditText) findViewById(R.id.etbrukernavn);
-        etpassord = (EditText) findViewById(R.id.etpassord);
+        edit_username = (EditText) findViewById(R.id.edit_username);
+        edit_password = (EditText) findViewById(R.id.edit_password);
         bLogin = (Button) findViewById(R.id.bLogin);
         bLogin.setOnClickListener(this);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.bLogin:
+                username = edit_username.getText().toString();
+                password = edit_password.getText().toString();
+                startActivity(new Intent(this, DisplayCoursesActivity.class));
+                break;
+            case R.id.button:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+    }
+}
+/*
         try{
             if (v.getId() == bLogin.getId()) {
-
-                String a= etbrukernavn.getText().toString();
-                String b=etpassord.getText().toString();
-                Database db= new Database();
-                db.setUsername(a);
-                db.setPassword(b);
-                db.execute();
-
-                //db.getCourses();
-                //System.err.println(db.getList());
-                //new Database().execute();
-                startActivity( new Intent(this, DisplayCoursesActivity.class));
-
-            }}
-        catch(Exception e){
+                String username = edit_username.getText().toString();
+                String password = edit_password.getText().toString();
+                User user = new User(username, password);
+                startActivity(new Intent(this, DisplayCoursesActivity.class));
+            } else if (v.getId() == button.getId()) {
+                startActivity(new Intent(this, RegisterUser.class));
+            }
+        }
+        catch(Exception e) {
             System.out.print(e);
         }
     }
 }
+*/
---
+++ app/src/main/java/com/example/ingridstoen/alarme/RegisterUser.java
@@ -0,0 +1,46 @@
+package com.example.ingridstoen.alarme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterUser extends Activity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText edit_username, edit_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        edit_username = (EditText) findViewById(R.id.edit_username);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        bRegister = (Button) findViewById(R.id.bRegister);
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        try{
+            if (v.getId() == bRegister.getId()) {
+                String username = edit_username.getText().toString();
+                String password = edit_password.getText().toString();
+                Database db = new Database();
+                db.setUsername(username);
+                db.setPassword(password);
+                db.execute();
+                String course1 = null;
+                String course2 = null;
+                String course3 = null;
+                String course4 = null;
+                // Gi de fire Stringene til intent
+                startActivity( new Intent(this, DisplayCoursesActivity.class));
+            }}
+        catch(Exception e){
+            System.out.print(e);
+        }
+    }
+}
