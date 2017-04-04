7de29afa7ca3697777ba45ec59c72e4954aab81f
ingridstoen <stoeningrid@gmail.com>
2017-04-04 14:40:56 +0200

    Lagt til dependencies som trengs ti testing

--- app/src/test/java/com/example/ingridstoen/alarme/ExampleUnitTest.java
+++ app/src/test/java/com/example/ingridstoen/alarme/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package com.example.ingridstoen.alarme;
 
+import android.view.View;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +13,15 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+    /*
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void login() throws Exception {
+        String username = "ingridbs";
+        String password = "In0292*s";
+        List<String> courses = new Database_Courses().execute().get();
+        assertEquals(new ArrayList<>(Arrays.asList("TTM4100", "TFY4125", "TDT4145")), new ArrayList(Arrays.asList(courses)));
     }
+    */
 }
